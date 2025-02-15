6e9647fdab63217af9bfb83f664db84e214be486
Divyansh bharbat <bharbatdivyansh1@gmail.com>
2025-02-15 15:56:01 +0530

    m

--- src/assets/Components/index.js
+++ src/assets/Components/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 
 // Use the CORS middleware with the specified options
 app.use(cors({
-  origin: "https://todolist-sknn.vercel.app",  // Allow your frontend domain
+  origin: "https://todolist-divyansh.vercel.app",  // Allow your frontend domain
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 }));
